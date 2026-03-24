cb6e3a0246d7ec0a29e8a05793a7321d65745535
Phillip Wood <PWood9@uclan.ac.uk>
2026-03-24 13:48:35 +0000

    changed a bunch and it now actually works

--- UITest.py
+++ UITest.py
@@ -1,9 +1,20 @@
 import UIV2
 import time
-
-updater:UIV2.Updater, ui:UIV2.UI = UIV2.Setup("Test", "Test Sutitle", "This is a Test", i, 100, 10)
-
-for i in range(0,100, 10):
-    time.sleep(1)
-    updater
+updater:UIV2.Updater
+ui:UIV2.UI
+max = 100
+updater, ui = UIV2.Init()
+running = True
+def Stop(updater, ui):
+    global running
+    UIV2.Cancel(updater, ui)
+    running = False
+while True:
+    ui.update()
+    if running:
+        for i in range(0,max+1, 10):
+            time.sleep(1)
+            ui.update()
+            UIV2.Update(updater, ui, "Test", "Test Subtitle", f"Log: {i}", i, "100 MB", max)
+        Stop(updater,ui)
         
--- UIV2.py
+++ UIV2.py
@@ -12,57 +12,75 @@ def cleanStr(string:str):
     return string.replace("\n", "").replace("\r", "")
 
 class UI:
-    
-     #---------- String Variables ---------
-    windowTitleStr:str = ""
-    titleStr:str = ""
-    subtitleStr:str = ""
-    logStr:str = ""
-    etaStr:str = ""
-    pValue:str = ""
-    elapsedStr:str = ""
-    memStr:str = ""
-    Width:str = 650
-    Height:str = 150
-    #---------- TK ----------
-    root = ttk.Tk()
-    root.minsize(Width,Height)
-    root.maxsize(Width,Height)
-    #--------- Fonts ----------
-    titleFont = font.Font(root, name="title", family="Segoe UI", size=12, weight='bold')
-    subtitleFont = font.Font(root, name="subtitle", family="Segoe UI", size=9, weight='normal')
-    defaultFont = font.Font(root, name="default", family="Segoe UI", size=9, weight='normal')
-    #---------- TK Variables --------
-    _titleVar = tk.StringVar()
-    _subtitleVar = tk.StringVar()
-    _logVar = tk.StringVar()
-    _etaVar = tk.StringVar()
-    _pVar = tk.IntVar()
-    _elapsedVar = tk.StringVar()
-    _memVar = tk.StringVar()
-    #--------- TK Widgets ----------
-    mainFrame = ttk.Frame(root)
-    titleFrame = ttk.Frame(mainFrame)
-    otherFrame = ttk.Frame(mainFrame)
-    title = ttk.Label(titleFrame, textvariable=_titleVar, font = titleFont)
-    subtitle =  ttk.Label(titleFrame, textvariable=_subtitleVar, font=subtitleFont, wraplength=Width-40)
-    pBar = ttk.Progressbar(otherFrame, variable=_pVar, length=Width-50)
-    log = ttk.Label(otherFrame, textvariable = _logVar, font=defaultFont)
-    eta = ttk.Label(otherFrame, textvariable=_etaVar, font=defaultFont)
-    elapsed = ttk.Label(otherFrame, textvariable=_elapsedVar, font=defaultFont)
-    mem = ttk.Label(otherFrame, textvariable=_memVar, font=defaultFont)
-    
-    
-    title.grid(sticky="nw", row=0)  
-    subtitle.grid(sticky="ew", row=1)
-    pBar.grid(sticky="w", row=2, pady=2)
-    log.grid(sticky="w", row=4)
-    eta.grid(sticky="e", row=4, pady=2)
-    elapsed.grid(sticky="nse", row=4, pady=2, padx=70)
-    mem.grid(sticky="w", row=5, pady=2)
+    def __init__(self):
+        #---------- String Variables ---------
+        self.windowTitleStr:str = "MakeMKV-Auto"
+        self.titleStr:str = "N/A"
+        self.subtitleStr:str = "N/A"
+        self.logStr:str = "N/A"
+        self.etaStr:str = "0:00:00"
+        self.pValue:str = "0"
+        self.elapsedStr:str = "0:00:00"
+        self.memStr:str = 0
+        self.Width:str = 650
+        self.Height:str = 150
+        #---------- TK ----------
+        self.root = ttk.Window(themename="darkly")
+        self.root.minsize(self.Width,self.Height)
+        self.root.maxsize(self.Width,self.Height)
+        #--------- Fonts ----------
+        self.titleFont = font.Font(self.root, name="title", family="Segoe UI", size=12, weight='bold')
+        self.subtitleFont = font.Font(self.root, name="subtitle", family="Segoe UI", size=9, weight='normal')
+        self.defaultFont = font.Font(self.root, name="default", family="Segoe UI", size=9, weight='normal')
+        #---------- TK Variables --------
+        self._titleVar = tk.StringVar()
+        self._subtitleVar = tk.StringVar()
+        self._logVar = tk.StringVar()
+        self._etaVar = tk.StringVar()
+        self._pVar = tk.IntVar()
+        self._elapsedVar = tk.StringVar()
+        self._memVar = tk.StringVar()
+        #--------- TK Widgets ----------
+        self.mainFrame = ttk.Frame(self.root)
+        self.titleFrame = ttk.Frame(self.mainFrame)
+        self.otherFrame = ttk.Frame(self.mainFrame)
+        self.title = ttk.Label(self.titleFrame, textvariable=self._titleVar, font = self.titleFont)
+        self.subtitle =  ttk.Label(self.titleFrame, textvariable=self._subtitleVar, font=self.subtitleFont, wraplength=self.Width-40)
+        self.pBar = ttk.Progressbar(self.otherFrame, variable=self._pVar, length=self.Width-50)
+        self.log = ttk.Label(self.otherFrame, textvariable = self._logVar, font=self.defaultFont)
+        self.eta = ttk.Label(self.otherFrame, textvariable=self._etaVar, font=self.defaultFont)
+        self.elapsed = ttk.Label(self.otherFrame, textvariable=self._elapsedVar, font=self.defaultFont)
+        self.mem = ttk.Label(self.otherFrame, textvariable=self._memVar, font=self.defaultFont)
+        
+        #place all of the widgets
+        self.mainFrame.pack(anchor="w", padx=10, pady=10, fill="both")
+        self.titleFrame.pack(fill="x", anchor="nw")
+        self.otherFrame.pack(fill="x", anchor="sw")
+        self.title.grid(sticky="nw", row=0)  
+        self.subtitle.grid(sticky="ew", row=1)
+        self.pBar.grid(sticky="w", row=2, pady=2)
+        self.log.grid(sticky="w", row=4)
+        self.eta.grid(sticky="e", row=4, pady=2)
+        self.elapsed.grid(sticky="nse", row=4, pady=2, padx=70)
+        self.mem.grid(sticky="w", row=5, pady=2)
        
-
-    def update(self):
+    def reset(self):
+        self.titleStr = "N/A"
+        self.subtitleStr = "N/A"
+        self.pValue = 0
+        self.logStr = "N\A"
+        self.etaStr = "0:00:00"
+        self.elapsedStr = "0:00:00"
+        self.memStr = "N/A"
+        self.windowTitleStr = "MakeMKV-Auto"
+        self.update()
+    def update(self):    
+        try: 
+            self.root.winfo_exists()
+        except:
+            print("Couldn't find the window... Closing")
+            quit()
+        #update widgets
         self._titleVar.set(self.titleStr)
         self._subtitleVar.set(self.subtitleStr)
         self._pVar.set(self.pValue)
@@ -72,24 +90,28 @@ class UI:
         self._memVar.set(self.memStr)
         self.root.title(self.windowTitleStr)
 
+        #run tk update loop
         self.root.update_idletasks()
         self.root.update()
         
-
+def Cancel(updater, ui):
+    del updater
+    ui.reset()
 #an instance of this class will be called every time we start a new task
 class Updater:
-    def __init__():
-        startTime = datetime.datetime.now()
-        last_time = datetime.datetime.now()
-        last_value = 0
-        last_speed = 0
-        path = "0"
-        total = "0"
-    #this will update all of the strings and progressBar
-    def Update(self, ui, title, subtitle, value, mem, max):
+    def __init__(self):
+        self.startTime = datetime.datetime.now()
+        self.last_time = datetime.datetime.now()
+        self.last_value = 0
+        self.last_speed = 0
+        self.path = ""
+        self.total = 0
+
+def Update(updater, ui, title, subtitle, log, value, mem, max):
         ui.titleStr = title
         ui.subtitleStr = subtitle
         ui.pValue = value
+        ui.logStr = log
         ui.memStr = mem
         # Source - https://stackoverflow.com/a/929104
         new_value = ((value - 0) / (max - 0) ) * (100 - 0) + 0
@@ -97,10 +119,10 @@ class Updater:
 
         print(f"Percentage: {new_value.__floor__()}%")
         # pgValue = new_value
-        # progress.set_progress(int(new_value))
+        # progress.setself._progress(int(new_value))
         #calculate average speed
-        valueDiff = new_value - self.last_value
-        timeDiff = datetime.datetime.now().timestamp() - self.last_time.timestamp()
+        valueDiff = new_value - updater.last_value
+        timeDiff = datetime.datetime.now().timestamp() - updater.last_time.timestamp()
         #we progressed valuediff amount in timeDiff time
         # e.g. 1 unit in 1 second
         
@@ -108,10 +130,10 @@ class Updater:
         smoothing = .1
         speed = valueDiff / timeDiff if timeDiff > 0 else 0
         if speed <=0:
-            smoothed_speed = self.last_speed
+            smoothed_speed = updater.last_speed
 
         else:
-            smoothed_speed = smoothing * speed + (1-smoothing) * self.last_speed
+            smoothed_speed = smoothing * speed + (1-smoothing) * updater.last_speed
         
         speed = smoothed_speed
 
@@ -128,11 +150,9 @@ class Updater:
 
 
         ui.windowTitleStr = f"{new_value.__floor__()}% | {ui.elapsedStr} | {ui.etaStr}"
-        self.last_value = new_value
-        self.last_time = datetime.datetime.now()
-        self.last_speed = speed
-
-
+        updater.last_value = new_value
+        updater.last_time = datetime.datetime.now()
+        updater.last_speed = speed
 def Init():
     updater = Updater()
     ui = UI()
